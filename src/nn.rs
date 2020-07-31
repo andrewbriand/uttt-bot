ab67d3fdb758221fcd2f23f7a465952e2bef34ae
Andrew Briand <atb8888@comcast.net>
2020-07-30 23:40:58 -0700

    Inference working

--- src/nn.rs
+++ src/nn.rs
@@ -6,7 +6,7 @@ pub struct Layer {
 }
 
 impl Layer {
-    fn new(_size: usize, _input_size: usize, _weights: Vec<f32>, _biases: Vec<f32>) -> Layer {
+    pub fn new(_size: usize, _input_size: usize, _weights: Vec<f32>, _biases: Vec<f32>) -> Layer {
         debug_assert!(_weights.len() == _input_size * _size);
         debug_assert!(_biases.len() == _size);
         Layer {
@@ -17,18 +17,20 @@ impl Layer {
         }
     }
 
-    fn vec_dot(v1: &[f32], v2: &[f32]) -> f32 {
+     fn vec_dot(v1: &[f32], v2: &[f32]) -> f32 {
+        //println!("v1 len: {}", v1.len());
+        //println!("v2 len: {}", v2.len());
         debug_assert!(v1.len() == v2.len());
         v1.iter().zip(v2).map(|(i1, i2)| i1 * i2).sum()
     }
 
-    fn eval(&self, input: &Vec<f32>) -> Vec<f32> {
+     fn eval(&self, input: &Vec<f32>) -> Vec<f32> {
         debug_assert!(input.len() == self.input_size);
         let mut result = Vec::with_capacity(self.size);
         // For every neuron:
         // multiply the input by the weights of that neuron (dot product)
         for i in 0..self.size {
-            result.push(Layer::vec_dot(&input[..], &self.weights[(i*self.size)..(i*(self.size + 1))]));
+            result.push(Layer::vec_dot(&input[..], &self.weights[(i*self.input_size)..((i+1)*(self.input_size))]));
         }
 
         // add the biases
@@ -41,13 +43,14 @@ pub struct NeuralNet {
 }
 
 impl NeuralNet {
-    fn new(_layers: Vec<Layer>) -> NeuralNet {
+    pub fn new(_layers: Vec<Layer>) -> NeuralNet {
         NeuralNet {
             layers: _layers,
         }
     }
 
-    fn infer(&self, input: &Vec<f32>) -> Vec<f32> {
+    pub fn infer(&self, input: &Vec<f32>) -> Vec<f32> {
+        debug_assert!(input.len() == self.layers[0].input_size);
         let mut curr_vec = self.layers[0].eval(input);
         for i in 1..self.layers.len() {
             curr_vec = self.layers[i].eval(&curr_vec);
